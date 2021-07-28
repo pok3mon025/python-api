0928775a167732c2bd6ba4c949a919a073f709f8
pok3mon025 <pok3mon025@gmail.com>
2021-07-28 23:43:47 +0900

    unit test

--- config.py
+++ config.py
@@ -7,3 +7,17 @@ db = {
 }
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
 JWT_SECRET_KEY = 'miniter'
+
+test_db = {
+    'user': 'root',
+    'password': 'root',
+    'host': 'localhost',
+    'port': 3306,
+    'database': 'minister_test'
+}
+
+test_config = {
+    'DB_URL': f"mysql+mysqlconnector://{test_db['user']}:{test_db['password']}@{test_db['host']}:{test_db['port']}/{test_db['database']}?charset=utf8",
+    'JWT_SECRET_KEY': 'miniter',
+    'JWT_EXP_DELTA_SECONDS': 7 * 24 * 60 * 60
+}
